81a1dc8a6e8e4c5b15e9ede3815fc2ef233535ad
gonzicz <gonzicz83@gmail.com>
2019-05-07 20:08:57 +0200

    dodanie controlera obsługującego widoki w html

---
+++ src/main/java/pl/sda/filmrepository/AllSuggestionsViewController.java
@@ -0,0 +1,25 @@
+package pl.sda.filmrepository;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+@RequestMapping("/app/suggestions")
+public class AllSuggestionsViewController {
+    SuggestioRepository repository;
+
+    public AllSuggestionsViewController(SuggestioRepository repository) {
+        this.repository = repository;
+    }
+
+    @GetMapping
+    String allSuggestions(Model model){
+        model.addAttribute("allSuggestions",repository.findAll());
+        return "suggestions";
+    }
+
+
+}
