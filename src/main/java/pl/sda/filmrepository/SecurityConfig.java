1dbe7e39ffe391e4dde5d0baf42bbca1d46b6a6c
gonzicz <gonzicz83@gmail.com>
2019-05-07 18:52:59 +0200

    dodanie prostego security

---
+++ src/main/java/pl/sda/filmrepository/SecurityConfig.java
@@ -0,0 +1,25 @@
+package pl.sda.filmrepository;
+
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.authorizeRequests().antMatchers("/api/suggestions/**")
+                .authenticated()
+                .and().httpBasic()
+                .and().csrf().disable();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication()
+                .withUser("admin")
+                .password("{noop}password")
+                .roles("ADMIN");
+    }
+}
